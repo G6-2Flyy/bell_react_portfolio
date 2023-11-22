db57e8c750d7b60c8b27172a988c43b35cbe7db2
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-11-22 06:04:22 -0600

    created components folder for header footer nav project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer>
+           <p>
+            <a href=""><ico src="" alt="" /></a>
+            <a href=""><img src="" alt="" /></a>
+            <a href=""><img src="" alt="" /></a>
+            </p>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Bell Portfolio</h1>
+        </header>
+    )
+}
