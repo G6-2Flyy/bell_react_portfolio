a02bf7d59605e9037dcafff45f313038385768f4
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-11-26 07:31:43 -0600

    added link to github page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
            <p>
-            <a href=""><i className="fa-brands fa-github"></i></a>
+            <a href="https://github.com/G6-2Flyy"><i className="fa-brands fa-github"></i></a>
             <a href=""><i className="fa-brands fa-linkedin"></i></a>
             <a href=""><i className="fa-solid fa-envelope"></i></a>
             </p>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,6 +2,7 @@ export default function Resume() {
     return (
         <div>
             <h3>Resume</h3> 
+            <p></p>
             <a href="/resume/RBell_Resume.pdf" download="Raymond Bell's Resume">Download Resume</a>
         </div>
     )
