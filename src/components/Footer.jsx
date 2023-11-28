bc22dd62a5b30a7cce7e73e392e4406ff450083d
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-11-27 22:04:50 -0600

    added linkedin link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer>
            <p>
             <a href="https://github.com/G6-2Flyy" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
-            <a href="" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a>
             <a href="https://stackoverflow.com/users/19529883/ray-b" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-stack-overflow"></i></a>
             </p>
             <p>&#169; Bell Portfolio 2023</p>
