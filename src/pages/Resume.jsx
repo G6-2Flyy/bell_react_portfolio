6397abe342ed14f0483f83ab4179bec362ab3f83
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2024-03-30 08:16:19 -0500

    updated pdf and resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,29 +1,49 @@
 export default function Resume() {
     return (
-        <div>
-            <h3>Resume</h3>
-            <a href="/resume/rbell_resume.pdf" download="Raymond Bell's Resume">Download Resume</a>
-            
-            <h3>Front-end Proficiencies</h3>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-                <li>jQuery</li>
-                <li>Responsive design</li>
-                <li>React</li>
-                <li>Bootstrap</li>
-            </ul>
-            <h3> Back-end Proficiencies</h3>
-            <ul>
-                <li>APIs</li>
-                <li>Node</li>
-                <li>Express</li>
-                <li>MySQL, Sequelize</li>
-                <li>MongoDB, Mongoose</li>
-                <li>REST</li>
-                <li>GraphQL</li>
-            </ul>
+        <div className="resume">
+             <a href="/resume/rbell_resume.pdf" download="Raymond Bell's Resume">Download Resume</a>
+            <h1>Raymond Bell</h1>
+            <p>Dallas, TX 75208 | Phone: 757.256.9950 | Email: rcarlb2@gmail.com</p>
+            <p>GitHub: <a href="https://github.com/G6-2Flyy" target="_blank" rel="noopener noreferrer">G6-2Flyy</a> | Portfolio: <a href="https://rbell-portfolio.netlify.app" target="_blank" rel="noopener noreferrer">rbell-portfolio.netlify.app</a></p>
+            <hr />
+            <h3>Summary</h3>
+            <p>Data Analyst with MERN stack skills and 20 years of experience in operations, logistics, and production management. Former decorated military officer with prior secret clearance and a passion for transforming large data to create web-based applications, visualizations and dashboards to drive business decision-making.</p>
+            <hr />
+            <h3>Technical Skills</h3>
+            <p><strong>Languages:</strong> Python, JavaScript ES6+, jQuery, R, SQL, HTML5, CSS </p>
+            <p><strong>Tools:</strong> MongoDB, MySQL, PostgreSQL, SQlite, SQLAlchemy, AWS, Flask, Tableau, Seaborn, Plotly, Matplotlib, TensorFlow, Pandas, NumPy, Matplotlib, Plotly, Leaflet.js, Scikit-Learn, Jupyter Notebook, React, Node.js, Express.js, Handlebars.js, GraphQL/Apollo, Insomnia, Bootstrap, Visual Paradigm, Visio, GitHub, Jest.js</p>
+            <p><strong>Skills:</strong> Data Modeling, Data Analytics, Data Visualization, Data Processing, Database Management, Web Development, Machine Learning </p>
+            <p><strong>Other:</strong> Microsoft Office Suite, SAP, Mini-tab, Project Management, Lean Six-Sigma</p>
+           <hr></hr>
+           <h3>Projects</h3>
+           <h5>Tech-E</h5>
+           <p><a href="https://still-fortress-07413-3ec76e5ba3ea.herokuapp.com" target="_blank" rel="noopener noreferrer">https://still-fortress-07413-3ec76e5ba3ea.herokuapp.com</a></p>
+           <ul>
+            <li>Summary: Tech-E is an e-commerce website created to provide consumers an alternate online option to purchase a wide range of tech products. </li>
+           <li>Role: Back-end development, including server, models, resolvers, and typedefs</li>
+           <li>Tools: React.js, Node.js, Tailwind CSS, MongoDB, Stripe, GraphQL/Apollo, JWT</li>
+           </ul>
+           <h5>STAR</h5>
+           <p><a href="https://quiet-spire-24074-96c577a8c013.herokuapp.com" target="_blank" rel="noopener noreferrer">https://quiet-spire-24074-96c577a8c013.herokuapp.com</a></p>
+           <ul>
+            <li>Summary: STAR is a full-stack web-based social media application designed to allow users to create, post, share, and interact with multimedia content.</li>
+           <li>Role: Back-end development, including routes, models, photos, and video integration</li>
+           <li>Tools: Node.js, Handlebars.js, BCrypt.js, Multer, Cloudinary, Tailwind CSS</li>
+           </ul>
+           <h5>Russia’s Invasion of Ukraine</h5>
+           <p><a href="https://g6-2flyy.github.io/Invasion_Of_Ukraine/" target="_blank" rel="noopener noreferrer">https://g6-2flyy.github.io/Invasion_Of_Ukraine/</a></p>
+           <ul>
+            <li>Summary: This project includes collaborative analysis of a data set that tracks all the different war incidents in Ukraine over the past year for interesting patterns and insights.</li>
+           <li>Role: Data cleaning, data wrangling, and filtering to create responsive data visualizations</li>
+           <li>Tools: CSS, Plotly, Leaflet.js, Pandas, NumPy, Highcharts, Apexcharts.js</li>
+           </ul>
+           <h5>SILICON PRAIRIE BANK</h5>
+           <p><a href="https://rbell-silicon-praire-bank.onrender.com/" target="_blank" rel="noopener noreferrer">https://rbell-silicon-praire-bank.onrender.com/</a></p>
+           <ul>
+            <li>Summary: This project includes collaborative analysis of a data set that tracks all the different war incidents in Ukraine over the past year for interesting patterns and insights.</li>
+           <li>Role: Data cleaning, data wrangling, and filtering to create responsive data visualizations</li>
+           <li>Tools: CSS, Plotly, Leaflet.js, Pandas, NumPy, Highcharts, Apexcharts.js</li>
+           </ul>
         </div>
     )
 }
