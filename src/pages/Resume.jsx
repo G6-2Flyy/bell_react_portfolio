dbe354d4eccfb5bccf8ca41eca50f93968fe3f82
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2024-03-30 09:34:44 -0500

    added remaining resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,8 +2,8 @@ export default function Resume() {
     return (
         <div className="resume">
              <a href="/resume/rbell_resume.pdf" download="Raymond Bell's Resume">Download Resume</a>
-            <h1>Raymond Bell</h1>
-            <p>Dallas, TX 75208 | Phone: 757.256.9950 | Email: rcarlb2@gmail.com</p>
+            {/* <h1>Raymond Bell</h1>
+            <p>Dallas, TX 75208 | Phone: xxx.xxx.xxxx | Email: rcarlb2@gmail.com</p>
             <p>GitHub: <a href="https://github.com/G6-2Flyy" target="_blank" rel="noopener noreferrer">G6-2Flyy</a> | Portfolio: <a href="https://rbell-portfolio.netlify.app" target="_blank" rel="noopener noreferrer">rbell-portfolio.netlify.app</a></p>
             <hr />
             <h3>Summary</h3>
@@ -40,10 +40,57 @@ export default function Resume() {
            <h5>SILICON PRAIRIE BANK</h5>
            <p><a href="https://rbell-silicon-praire-bank.onrender.com/" target="_blank" rel="noopener noreferrer">https://rbell-silicon-praire-bank.onrender.com/</a></p>
            <ul>
-            <li>Summary: This project includes collaborative analysis of a data set that tracks all the different war incidents in Ukraine over the past year for interesting patterns and insights.</li>
-           <li>Role: Data cleaning, data wrangling, and filtering to create responsive data visualizations</li>
-           <li>Tools: CSS, Plotly, Leaflet.js, Pandas, NumPy, Highcharts, Apexcharts.js</li>
+            <li>Summary: SPB is a web application that uses a machine learning model to determine consumer loan and credit card application approvals.</li>
+           <li>Role: Data cleaning, data wrangling, data visualization, and machine learning models</li>
+           <li>Tools: Pandas, CSS, Flask, Tableau, TensorFlow</li>
            </ul>
+           <hr/> */}
+           <h2>Experience</h2>
+           <h3>Continuing Career Education <span>May 2020-Present</span></h3>
+           <p>Full Stack Coding | Data Science | MBA <span>Dallas, TX</span></p>
+           <ul>
+            <li>Completed two intensive programs focused on attaining technical programming skills</li>
+           <li>Finalizing requirements for MBA and leveraging coding  and data science skills to enhance business analytics concentration</li>
+           </ul>
+           <h3>Amazon<span>Mar 2018-Apr 2020</span></h3>
+           <p>Analyst, E-Commerce  <span>Farmers Branch, TX</span></p>
+           <ul>
+            <li>Partnered with site leadership to apply critical thinking skills and analytical methods to achieve business goals and objectives</li>
+           <li>Re-engineered business processes and trained over 100 new hires resulting in faster product delivery and increased order completion percentages</li>
+           </ul>
+           <h3>LEAR CORPORATION<span>Jul 2014-Aug 2015</span></h3>
+           <p>Quality Manager, Automotive <span>Arlington, TX</span></p>
+           <ul>
+            <li>Used diagnostic software for functional testing, calibration, and defect resolution</li>
+           <li>Applied quantitative and qualitative analytical methods to generate key performance data inputs into Quality Management Systems to ensure stakeholder satisfaction</li>
+           </ul>
+           <h3>KRAFT- HEINZ<span>Aug 2013-Jun 2014</span></h3>
+           <p>Production Manager, Consumer Packaged Goods<span>Garland, TX</span></p>
+           <ul>
+            <li>Consistently acknowledged for achieving safety, quality, and cost KPIs for one of the largest food and beverage companies in the world</li>
+           <li>Analyzed company workflow design and applied Agile Methodologies (ILSS) to reduce product changeover time by 75% resulting in adoption as new standard for the business</li>
+           </ul>
+           <hr />
+           <h2>Additional Experience</h2>
+           <p><h3>The J.M. SMUCKER COMPANY</h3>Operations Manager, Consumer Packaged Goods</p>
+           <ul>
+            <li>Managed end-to-end product supply chain for the Folgers Instant Coffee Business Unit</li>
+           </ul>
+           <p><h3>DELL, INC.,</h3>Operations Manager, Technology</p>
+           <ul>
+            <li>Managed computer system development process for largest Manufacturing Center</li>
+           </ul>
+           <p>US ARMY, Pilot | Sr. Executive Officer | Chief Logistics Officer | Operations Officer, Aviation</p>
+           <ul>
+            <li>Used global intelligence to execute missions to achieve joint strategic military objectives</li>
+           </ul>
+           <hr />
+           <h2>Education</h2>
+           <p>Certificate, Full Stack Coding | Southern Methodist University | Dallas, TX</p>
+           <p>Certificate, Data Science | Southern Methodist University | Dallas, TX</p>
+           <p>Master of Business Administration | The University of Texas at Dallas | Dallas, TX</p>
+           <p>Master Certificate, Lean Six Sigma | Villanova University | Villanova, PA</p>
+           <p>Bachelor of Science, Accounting | The University of Tampa | Tampa, FL</p>
         </div>
     )
 }
