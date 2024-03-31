320e0efd77c54802d171453c5c47b0d40d9504e3
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2024-03-31 11:21:39 -0500

    more updates to resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,96 +1,284 @@
 export default function Resume() {
-    return (
-        <div className="resume">
-             <a href="/resume/rbell_resume.pdf" download="Raymond Bell's Resume">Download Resume</a>
-            {/* <h1>Raymond Bell</h1>
-            <p>Dallas, TX 75208 | Phone: xxx.xxx.xxxx | Email: rcarlb2@gmail.com</p>
-            <p>GitHub: <a href="https://github.com/G6-2Flyy" target="_blank" rel="noopener noreferrer">G6-2Flyy</a> | Portfolio: <a href="https://rbell-portfolio.netlify.app" target="_blank" rel="noopener noreferrer">rbell-portfolio.netlify.app</a></p>
-            <hr />
-            <h3>Summary</h3>
-            <p>Data Analyst with MERN stack skills and 20 years of experience in operations, logistics, and production management. Former decorated military officer with prior secret clearance and a passion for transforming large data to create web-based applications, visualizations and dashboards to drive business decision-making.</p>
-            <hr />
-            <h3>Technical Skills</h3>
-            <p><strong>Languages:</strong> Python, JavaScript ES6+, jQuery, R, SQL, HTML5, CSS </p>
-            <p><strong>Tools:</strong> MongoDB, MySQL, PostgreSQL, SQlite, SQLAlchemy, AWS, Flask, Tableau, Seaborn, Plotly, Matplotlib, TensorFlow, Pandas, NumPy, Matplotlib, Plotly, Leaflet.js, Scikit-Learn, Jupyter Notebook, React, Node.js, Express.js, Handlebars.js, GraphQL/Apollo, Insomnia, Bootstrap, Visual Paradigm, Visio, GitHub, Jest.js</p>
-            <p><strong>Skills:</strong> Data Modeling, Data Analytics, Data Visualization, Data Processing, Database Management, Web Development, Machine Learning </p>
-            <p><strong>Other:</strong> Microsoft Office Suite, SAP, Mini-tab, Project Management, Lean Six-Sigma</p>
-           <hr></hr>
-           <h3>Projects</h3>
-           <h5>Tech-E</h5>
-           <p><a href="https://still-fortress-07413-3ec76e5ba3ea.herokuapp.com" target="_blank" rel="noopener noreferrer">https://still-fortress-07413-3ec76e5ba3ea.herokuapp.com</a></p>
-           <ul>
-            <li>Summary: Tech-E is an e-commerce website created to provide consumers an alternate online option to purchase a wide range of tech products. </li>
-           <li>Role: Back-end development, including server, models, resolvers, and typedefs</li>
-           <li>Tools: React.js, Node.js, Tailwind CSS, MongoDB, Stripe, GraphQL/Apollo, JWT</li>
-           </ul>
-           <h5>STAR</h5>
-           <p><a href="https://quiet-spire-24074-96c577a8c013.herokuapp.com" target="_blank" rel="noopener noreferrer">https://quiet-spire-24074-96c577a8c013.herokuapp.com</a></p>
-           <ul>
-            <li>Summary: STAR is a full-stack web-based social media application designed to allow users to create, post, share, and interact with multimedia content.</li>
-           <li>Role: Back-end development, including routes, models, photos, and video integration</li>
-           <li>Tools: Node.js, Handlebars.js, BCrypt.js, Multer, Cloudinary, Tailwind CSS</li>
-           </ul>
-           <h5>Russia’s Invasion of Ukraine</h5>
-           <p><a href="https://g6-2flyy.github.io/Invasion_Of_Ukraine/" target="_blank" rel="noopener noreferrer">https://g6-2flyy.github.io/Invasion_Of_Ukraine/</a></p>
-           <ul>
-            <li>Summary: This project includes collaborative analysis of a data set that tracks all the different war incidents in Ukraine over the past year for interesting patterns and insights.</li>
-           <li>Role: Data cleaning, data wrangling, and filtering to create responsive data visualizations</li>
-           <li>Tools: CSS, Plotly, Leaflet.js, Pandas, NumPy, Highcharts, Apexcharts.js</li>
-           </ul>
-           <h5>SILICON PRAIRIE BANK</h5>
-           <p><a href="https://rbell-silicon-praire-bank.onrender.com/" target="_blank" rel="noopener noreferrer">https://rbell-silicon-praire-bank.onrender.com/</a></p>
-           <ul>
-            <li>Summary: SPB is a web application that uses a machine learning model to determine consumer loan and credit card application approvals.</li>
-           <li>Role: Data cleaning, data wrangling, data visualization, and machine learning models</li>
-           <li>Tools: Pandas, CSS, Flask, Tableau, TensorFlow</li>
-           </ul>
-           <hr/> */}
-           <h2>Experience</h2>
-           <h3>Continuing Career Education <span>May 2020-Present</span></h3>
-           <p>Full Stack Coding | Data Science | MBA <span>Dallas, TX</span></p>
-           <ul>
-            <li>Completed two intensive programs focused on attaining technical programming skills</li>
-           <li>Finalizing requirements for MBA and leveraging coding  and data science skills to enhance business analytics concentration</li>
-           </ul>
-           <h3>Amazon<span>Mar 2018-Apr 2020</span></h3>
-           <p>Analyst, E-Commerce  <span>Farmers Branch, TX</span></p>
-           <ul>
-            <li>Partnered with site leadership to apply critical thinking skills and analytical methods to achieve business goals and objectives</li>
-           <li>Re-engineered business processes and trained over 100 new hires resulting in faster product delivery and increased order completion percentages</li>
-           </ul>
-           <h3>LEAR CORPORATION<span>Jul 2014-Aug 2015</span></h3>
-           <p>Quality Manager, Automotive <span>Arlington, TX</span></p>
-           <ul>
-            <li>Used diagnostic software for functional testing, calibration, and defect resolution</li>
-           <li>Applied quantitative and qualitative analytical methods to generate key performance data inputs into Quality Management Systems to ensure stakeholder satisfaction</li>
-           </ul>
-           <h3>KRAFT- HEINZ<span>Aug 2013-Jun 2014</span></h3>
-           <p>Production Manager, Consumer Packaged Goods<span>Garland, TX</span></p>
-           <ul>
-            <li>Consistently acknowledged for achieving safety, quality, and cost KPIs for one of the largest food and beverage companies in the world</li>
-           <li>Analyzed company workflow design and applied Agile Methodologies (ILSS) to reduce product changeover time by 75% resulting in adoption as new standard for the business</li>
-           </ul>
-           <hr />
-           <h2>Additional Experience</h2>
-           <p><h3>The J.M. SMUCKER COMPANY</h3>Operations Manager, Consumer Packaged Goods</p>
-           <ul>
-            <li>Managed end-to-end product supply chain for the Folgers Instant Coffee Business Unit</li>
-           </ul>
-           <p><h3>DELL, INC.,</h3>Operations Manager, Technology</p>
-           <ul>
-            <li>Managed computer system development process for largest Manufacturing Center</li>
-           </ul>
-           <p>US ARMY, Pilot | Sr. Executive Officer | Chief Logistics Officer | Operations Officer, Aviation</p>
-           <ul>
-            <li>Used global intelligence to execute missions to achieve joint strategic military objectives</li>
-           </ul>
-           <hr />
-           <h2>Education</h2>
-           <p>Certificate, Full Stack Coding | Southern Methodist University | Dallas, TX</p>
-           <p>Certificate, Data Science | Southern Methodist University | Dallas, TX</p>
-           <p>Master of Business Administration | The University of Texas at Dallas | Dallas, TX</p>
-           <p>Master Certificate, Lean Six Sigma | Villanova University | Villanova, PA</p>
-           <p>Bachelor of Science, Accounting | The University of Tampa | Tampa, FL</p>
-        </div>
-    )
+  return (
+    <div className="resume">
+    
+      <section>
+        <header>
+        <h1>Raymond Bell</h1>
+        <p>Dallas, TX 75208 | Phone: xxx.xxx.xxxx | Email: rcarlb2@gmail.com</p>
+        <p>
+          GitHub:{" "}
+          <a
+            href="https://github.com/G6-2Flyy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            G6-2Flyy
+          </a>{" "}
+          | Portfolio:{" "}
+          <a
+            href="https://rbell-portfolio.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            rbell-portfolio.netlify.app
+          </a>
+        </p>
+        </header>
+        
+        <hr />
+        <h3>Summary</h3>
+        <p>
+          Data Analyst with MERN stack skills and 20 years of experience in
+          operations, logistics, and production management. Former decorated
+          military officer with prior secret clearance and a passion for
+          transforming large data to create web-based applications,
+          visualizations and dashboards to drive business decision-making.
+        </p>
+        <hr />
+        <h3>Technical Skills</h3>
+        <p>
+          <strong>Languages:</strong> Python, JavaScript ES6+, jQuery, R, SQL,
+          HTML5, CSS{" "}
+        </p>
+        <p>
+          <strong>Tools:</strong> MongoDB, MySQL, PostgreSQL, SQlite,
+          SQLAlchemy, AWS, Flask, Tableau, Seaborn, Plotly, Matplotlib,
+          TensorFlow, Pandas, NumPy, Matplotlib, Plotly, Leaflet.js,
+          Scikit-Learn, Jupyter Notebook, React, Node.js, Express.js,
+          Handlebars.js, GraphQL/Apollo, Insomnia, Bootstrap, Visual Paradigm,
+          Visio, GitHub, Jest.js
+        </p>
+        <p>
+          <strong>Skills:</strong> Data Modeling, Data Analytics, Data
+          Visualization, Data Processing, Database Management, Web Development,
+          Machine Learning{" "}
+        </p>
+        <p>
+          <strong>Other:</strong> Microsoft Office Suite, SAP, Mini-tab, Project
+          Management, Lean Six-Sigma
+        </p>
+        <hr></hr>
+        <h3>Projects</h3>
+        <h5 className="project-name">Tech-E</h5>
+        <p>
+          <a
+            href="https://still-fortress-07413-3ec76e5ba3ea.herokuapp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://still-fortress-07413-3ec76e5ba3ea.herokuapp.com
+          </a>
+        </p>
+        <ul className="project-bullets" >
+          <li>
+            <strong>Summary:</strong> Tech-E is an e-commerce website created to provide
+            consumers an alternate online option to purchase a wide range of
+            tech products.{" "}
+          </li>
+          <li>
+            <strong>Role:</strong> Back-end development, including server, models, resolvers, and
+            typedefs
+          </li>
+          <li>
+            <strong>Tools:</strong> React.js, Node.js, Tailwind CSS, MongoDB, Stripe,
+            GraphQL/Apollo, JWT
+          </li>
+        </ul>
+        <h5 className="project-name">STAR</h5>
+        <p>
+          <a
+            href="https://quiet-spire-24074-96c577a8c013.herokuapp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://quiet-spire-24074-96c577a8c013.herokuapp.com
+          </a>
+        </p>
+        <ul>
+          <li>
+            <strong>Summary:</strong> STAR is a full-stack web-based social media application
+            designed to allow users to create, post, share, and interact with
+            multimedia content.
+          </li>
+          <li>
+            <strong>Role:</strong> Back-end development, including routes, models, photos, and
+            video integration
+          </li>
+          <li>
+            <strong>Tools:</strong> Node.js, Handlebars.js, BCrypt.js, Multer, Cloudinary,
+            Tailwind CSS
+          </li>
+        </ul>
+        <h5 className="project-name">Russia’s Invasion of Ukraine</h5>
+        <p>
+          <a
+            href="https://g6-2flyy.github.io/Invasion_Of_Ukraine/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://g6-2flyy.github.io/Invasion_Of_Ukraine/
+          </a>
+        </p>
+        <ul>
+          <li>
+            <strong>Summary:</strong> This project includes collaborative analysis of a data set
+            that tracks all the different war incidents in Ukraine over the past
+            year for interesting patterns and insights.
+          </li>
+          <li>
+            <strong>Role:</strong> Data cleaning, data wrangling, and filtering to create
+            responsive data visualizations
+          </li>
+          <li>
+            <strong>Tools:</strong> CSS, Plotly, Leaflet.js, Pandas, NumPy, Highcharts,
+            Apexcharts.js
+          </li>
+        </ul>
+        <h5 className="project-name">SILICON PRAIRIE BANK</h5>
+        <p>
+          <a
+            href="https://rbell-silicon-praire-bank.onrender.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://rbell-silicon-praire-bank.onrender.com/
+          </a>
+        </p>
+        <ul>
+          <li>
+            <strong>Summary:</strong> SPB is a web application that uses a machine learning model
+            to determine consumer loan and credit card application approvals.
+          </li>
+          <li>
+            <strong>Role:</strong> Data cleaning, data wrangling, data visualization, and machine
+            learning models
+          </li>
+          <li>
+            <strong>Tools:</strong> Pandas, CSS, Flask, Tableau, TensorFlow
+         </li>
+        </ul>
+        <hr />
+        <h3>Experience</h3>
+        <h4 className="space-between">
+          Continuing Career Education <span>May 2020-Present</span>
+        </h4>
+        <p className="space-between">
+          Full Stack Coding | Data Science | MBA <span>Dallas, TX</span>
+        </p>
+        <ul>
+          <li>
+            Completed two intensive programs focused on attaining technical
+            programming skills
+          </li>
+          <li>
+            Finalizing requirements for MBA and leveraging coding and data
+            science skills to enhance business analytics concentration
+          </li>
+        </ul>
+        <h4 className="space-between">
+          Amazon<span>Mar 2018-Apr 2020</span>
+        </h4>
+        <p className="space-between">
+          Analyst, E-Commerce <span>Farmers Branch, TX</span>
+        </p>
+        <ul>
+          <li>
+            Partnered with site leadership to apply critical thinking skills and
+            analytical methods to achieve business goals and objectives
+          </li>
+          <li>
+            Re-engineered business processes and trained over 100 new hires
+            resulting in faster product delivery and increased order completion
+            percentages
+          </li>
+        </ul>
+        <h4 className="space-between">
+          LEAR CORPORATION<span>Jul 2014-Aug 2015</span>
+        </h4>
+        <p className="space-between">
+          Quality Manager, Automotive <span>Arlington, TX</span>
+        </p>
+        <ul>
+          <li>
+            Used diagnostic software for functional testing, calibration, and
+            defect resolution
+          </li>
+          <li>
+            Applied quantitative and qualitative analytical methods to generate
+            key performance data inputs into Quality Management Systems to
+            ensure stakeholder satisfaction
+          </li>
+        </ul>
+        <h4 className="space-between">
+          KRAFT- HEINZ<span>Aug 2013-Jun 2014</span>
+        </h4>
+        <p className="space-between">
+          Production Manager, Consumer Packaged Goods<span>Garland, TX</span>
+        </p>
+        <ul>
+          <li>
+            Consistently acknowledged for achieving safety, quality, and cost
+            KPIs for one of the largest food and beverage companies in the world
+          </li>
+          <li>
+            Analyzed company workflow design and applied Agile Methodologies
+            (ILSS) to reduce product changeover time by 75% resulting in
+            adoption as new standard for the business
+          </li>
+        </ul>
+        <hr />
+        <h3>Additional Experience</h3>
+        <p>
+          <strong>The J.M. SMUCKER COMPANY, </strong>Operations Manager, <strong>Consumer Packaged Goods</strong>
+        </p>
+        <ul>
+          <li>
+            Managed end-to-end product supply chain for the Folgers Instant
+            Coffee Business Unit
+          </li>
+        </ul>
+        <p>
+          <strong>DELL, INC., </strong>Operations Manager, <strong>Technology</strong>
+        </p>
+        <ul>
+          <li>
+            Managed computer system development process for largest
+            Manufacturing Center
+          </li>
+        </ul>
+        <p>
+          <strong>US ARMY, </strong> Pilot | Sr. Executive Officer | Chief
+          Logistics Officer | Operations Officer, <strong>Aviation</strong>
+        </p>
+        <ul>
+          <li>
+            Used global intelligence to execute missions to achieve joint
+            strategic military objectives
+          </li>
+        </ul>
+        <hr />
+        <h3>Education</h3>
+        <p>
+          Certificate, Full Stack Coding | Southern Methodist University |
+          Dallas, TX
+        </p>
+        <p>
+          Certificate, Data Science | Southern Methodist University | Dallas, TX
+        </p>
+        <p>
+          Master of Business Administration | The University of Texas at Dallas
+          | Dallas, TX
+        </p>
+        <p>
+          Master Certificate, Lean Six Sigma | Villanova University | Villanova,
+          PA
+        </p>
+        <p>
+          Bachelor of Science, Accounting | The University of Tampa | Tampa, FL
+        </p>
+      </section>
+      <a href="/resume/rbell_resume.pdf" download="Raymond Bell's Resume">Download Resume</a>
+    </div>
+  );
 }
